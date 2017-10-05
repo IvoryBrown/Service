40305045db13d5d919a2556f4beb2af9c0ad2303
IvoryBrown <paradisho@gmail.com>
2017-10-05 21:07:16 +0200

    .

--- SoftService/Sql/Service.sql
+++ SoftService/Sql/Service.sql
@@ -15,31 +15,6 @@ CREATE SCHEMA IF NOT EXISTS `Service` DEFAULT CHARACTER SET utf8 COLLATE utf8_hu
 USE `Service` ;
 
 -- -----------------------------------------------------
--- Table `Service`.`image_alkatresz`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Service`.`image_alkatresz` (
-  `Image_a` LONGBLOB NULL,
-  `uj_alaktresz_azonosito_a` INT NOT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_hungarian_ci;
-
-
--- -----------------------------------------------------
--- Table `Service`.`software`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Service`.`software` (
-  `ID_sofware` INT NOT NULL AUTO_INCREMENT,
-  `software` VARCHAR(255) NULL,
-  `megjegyzes_s` VARCHAR(255) NULL,
-  `gepadatok_azonosito_g` INT NOT NULL,
-  PRIMARY KEY (`ID_sofware`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_hungarian_ci;
-
-
--- -----------------------------------------------------
 -- Table `Service`.`megrendelo`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Service`.`megrendelo` (
@@ -62,19 +37,50 @@ CREATE TABLE IF NOT EXISTS `Service`.`gepadatok` (
   `ID_g` INT NOT NULL AUTO_INCREMENT,
   `ugyfel_nev` VARCHAR(255) NOT NULL,
   `eszkoz_g` VARCHAR(255) NOT NULL,
+  `tipus` VARCHAR(255) NOT NULL,
   `sorozatszam_g` VARCHAR(255) NOT NULL,
   `allapot` VARCHAR(255) NOT NULL,
   `prioritas` VARCHAR(60) NULL,
-  `rogzites` DATETIME NOT NULL,
+  `vasarlas_ido` DATE NULL,
+  `rogzites` DATE NOT NULL,
   `hatarido` DATE NOT NULL,
-  `teljesitve` DATETIME NULL,
-  `megjegyzes_g` VARCHAR(255) NULL,
+  `teljesitve` DATE NULL,
+  `softwer` VARCHAR(255) NOT NULL,
+  `hardwer` VARCHAR(255) NOT NULL,
+  `takaritas` VARCHAR(255) NOT NULL,
+  `jelszo` VARCHAR(45) NULL,
+  `tartozekok` VARCHAR(255) NULL,
+  `serules` VARCHAR(255) NULL,
+  `hiba_leiras` VARCHAR(255) NOT NULL,
+  `valos_hiba` VARCHAR(255) NOT NULL,
   `megrendelo_ID_m` INT NOT NULL,
   PRIMARY KEY (`ID_g`),
   INDEX `fk_gepadatok_megrendelo_idx` (`megrendelo_ID_m` ASC),
   CONSTRAINT `fk_gepadatok_megrendelo`
     FOREIGN KEY (`megrendelo_ID_m`)
     REFERENCES `Service`.`megrendelo` (`ID_m`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_hungarian_ci;
+
+
+-- -----------------------------------------------------
+-- Table `Service`.`software`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Service`.`software` (
+  `ID_sofware` INT NOT NULL AUTO_INCREMENT,
+  `eszkoz_nev_s` VARCHAR(255) NULL,
+  `serial` VARCHAR(255) NULL,
+  `software` VARCHAR(255) NULL,
+  `megjegyzes_s` VARCHAR(255) NULL,
+  `gepadatok_ID_gs` INT NOT NULL,
+  PRIMARY KEY (`ID_sofware`),
+  INDEX `fk_software_gepadatok1_idx` (`gepadatok_ID_gs` ASC),
+  CONSTRAINT `fk_software_gepadatok1`
+    FOREIGN KEY (`gepadatok_ID_gs`)
+    REFERENCES `Service`.`gepadatok` (`ID_g`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -105,52 +111,23 @@ COLLATE = utf8_hungarian_ci;
 
 
 -- -----------------------------------------------------
--- Table `Service`.`uj_alaktresz`
+-- Table `Service`.`alkatresz`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Service`.`uj_alaktresz` (
+CREATE TABLE IF NOT EXISTS `Service`.`alkatresz` (
   `ID_a` INT NOT NULL,
-  `azonosito_a` INT NOT NULL,
-  `eszkoz_a` VARCHAR(255) NULL,
+  `eszkoz_nev_a` VARCHAR(255) NULL,
+  `eszkoz_serial_a` VARCHAR(255) NULL,
+  `alkatresz_tipus` VARCHAR(255) NULL,
+  `alkatresz_serial` VARCHAR(255) NULL,
   `megjegyzes_a` VARCHAR(255) NULL,
-  `gepadatok_azonosito_g` INT NOT NULL,
-  PRIMARY KEY (`azonosito_a`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_hungarian_ci;
-
-
--- -----------------------------------------------------
--- Table `Service`.`hasznalt_alaktresz`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Service`.`hasznalt_alaktresz` (
-  `ID_h` INT NOT NULL,
-  `azonosito_h` INT NOT NULL,
-  `eszkoz_h` VARCHAR(255) NULL,
-  `megjegyzes_h` VARCHAR(255) NULL,
-  `gepadatok_azonosito_g` INT NOT NULL,
-  PRIMARY KEY (`azonosito_h`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_hungarian_ci;
-
-
--- -----------------------------------------------------
--- Table `Service`.`image_hasznalt`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Service`.`image_hasznalt` (
-  `Image_h` LONGBLOB NULL,
-  `hasznalt_alaktresz_azonosito_h` INT NOT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_hungarian_ci;
-
-
--- -----------------------------------------------------
--- Table `Service`.`szervizes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Service`.`szervizes` (
-  `szervizes` VARCHAR(255) NOT NULL,
-  `gepadatok_azonosito_g` INT NOT NULL);
+  `gepadatok_ID_ga` INT NOT NULL,
+  PRIMARY KEY (`ID_a`),
+  INDEX `fk_alkatresz_gepadatok1_idx` (`gepadatok_ID_ga` ASC),
+  CONSTRAINT `fk_alkatresz_gepadatok1`
+    FOREIGN KEY (`gepadatok_ID_ga`)
+    REFERENCES `Service`.`gepadatok` (`ID_g`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
